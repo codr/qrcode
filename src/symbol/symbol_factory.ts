cea6a1659556bf8ddedbe13ee7994f25eab4acf1
Cody Coljee-Gray <cody.cg@gmail.com>
2025-06-22 11:52:26 -0700

    chore: remove unused functions in symbol factory

--- src/symbol/symbol_factory.ts
+++ src/symbol/symbol_factory.ts
@@ -8,18 +8,3 @@ export function createFromArray(array: number[][]): QRCode {
     return QRCode.fromArray(array);
   }
 }
-
-function createFromString(string: string): QRCode {
-  // 1. Check if Alphanumeric encoding can be used
-  // TODO(codr): Implement logic to check if the string can be encoded in Alphanumeric format
-
-  // 2. Use byte encoding if alphanumeric is not possible
-
-  const array = string.split('\n').map((line) =>
-    line
-      .split(',')
-      .map((value) => value.trim())
-      .map((value) => Number(value)),
-  );
-  return createFromArray(array);
-}
