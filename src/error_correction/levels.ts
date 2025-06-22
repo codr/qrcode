4631987cad33972a02843b7d2a86151504e5be24
Cody Coljee-Gray <cody.cg@gmail.com>
2025-06-22 11:43:01 -0700

    feat: added function for checking the error correction bits

--- src/error_correction/levels.ts
+++ src/error_correction/levels.ts
@@ -1,7 +1,23 @@
-
 export enum ErrorCorrectionLevel {
   L = 'L', // Low
   M = 'M', // Medium
   Q = 'Q', // Quartile
-  H = 'H'  // High
+  H = 'H', // High
+}
+
+export function getErrorCorrectionLevelBits(
+  level: ErrorCorrectionLevel,
+): number {
+  switch (level) {
+    case ErrorCorrectionLevel.L:
+      return 0b01; // 01 for Low
+    case ErrorCorrectionLevel.M:
+      return 0b00; // 00 for Medium
+    case ErrorCorrectionLevel.Q:
+      return 0b11; // 11 for Quartile
+    case ErrorCorrectionLevel.H:
+      return 0b10; // 10 for High
+    default:
+      throw new Error(`Unknown error correction level: ${level}`);
+  }
 }
