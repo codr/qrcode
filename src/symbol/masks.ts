35474c6219a49a1d1cf092be65553ce83b608165
Cody Coljee-Gray <cody.cg@gmail.com>
2025-06-25 09:49:12 -0700

    refactor: update masks.ts to be less verbose

--- src/symbol/masks.ts
+++ src/symbol/masks.ts
@@ -2,46 +2,34 @@ type MaskFunction = (x: number, y: number) => boolean;
 
 export type MaskPattern = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
 
+const PATTERN_MAP: Record<MaskPattern, MaskFunction> = {
+  0b000: (x, y) => (x + y) % 2 === 0,
+  0b001: (x, y) => y % 2 === 0,
+  0b010: (x, y) => x % 3 === 0,
+  0b011: (x, y) => (x + y) % 3 === 0,
+  0b100: (x, y) => (Math.floor(y / 2) + Math.floor(x / 3)) % 2 === 0,
+  0b101: (x, y) => ((y * x) % 2) + ((y * x) % 3) === 0,
+  0b110: (x, y) => (((x * y) % 2) + ((x * y) % 3)) % 2 === 0,
+  0b111: (x, y) => (((x * y) % 3) + ((x + y) % 2)) % 2 === 0,
+};
+
 export function getMaskPattern(maskPattern: MaskPattern): MaskFunction {
-  switch (maskPattern) {
-    case 0b000:
-      return (x, y) => (x + y) % 2 === 0;
-    case 0b001:
-      return (x, y) => y % 2 === 0;
-    case 0b010:
-      return (x, y) => x % 3 === 0;
-    case 0b011:
-      return (x, y) => (x + y) % 3 === 0;
-    case 0b100:
-      return (x, y) => (Math.floor(y / 2) + Math.floor(x / 3)) % 2 === 0;
-    case 0b101:
-      return (x, y) => ((y * x) % 2) + ((y * x) % 3) === 0;
-    case 0b110:
-      return (x, y) => (((x * y) % 2) + ((x * y) % 3)) % 2 === 0;
-    case 0b111:
-      return (x, y) => (((x * y) % 3) + ((x + y) % 2)) % 2 === 0;
-    default:
-      throw new Error('Invalid mask pattern: ' + maskPattern);
-  }
+  return PATTERN_MAP[maskPattern];
 }
 
 type MicroMaskPattern = 0 | 1 | 2 | 3;
 
+const MICRO_PATTERN_MAP: Record<MicroMaskPattern, MaskFunction> = {
+  0: getMaskPattern(1),
+  1: getMaskPattern(4),
+  2: getMaskPattern(6),
+  3: getMaskPattern(7),
+};
+
 export function getMicroMaskPattern(
   maskPattern: MicroMaskPattern,
 ): MaskFunction {
-  switch (maskPattern) {
-    case 0:
-      return getMaskPattern(1);
-    case 1:
-      return getMaskPattern(4);
-    case 2:
-      return getMaskPattern(6);
-    case 3:
-      return getMaskPattern(7);
-    default:
-      throw new Error('Invalid mask pattern: ' + maskPattern);
-  }
+  return MICRO_PATTERN_MAP[maskPattern];
 }
 
 export const formattingMask = 0b101010000010010;
